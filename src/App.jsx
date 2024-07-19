27cc30be94563d8aeb006032f030bdd137456378
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2024-07-19 22:03:11 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,48 +5,20 @@ import About from './Components/About/About'
 import MyWork from './Components/MyWork/MyWork'
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
-import React, { useRef, useState,useEffect } from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
-import { gsap } from 'gsap';
-function App() {
-  useEffect(() => {
-    // Initialize Locomotive Scroll
-    const scroll = new LocomotiveScroll({
-      el: document.querySelector('#main'),
-      smooth: true
-    });
-    const tl = gsap.timeline();
-    tl.to("#home",{
-      y:"100vh",
-      scale:0.5,
-      duration:0
-  })
-  tl.to("#home",{
-      y:"40vh",
-      duration:1,
-      delay:1
-  })
-  tl.to("#home",{
-      y:"0vh",
-      rotate:360,
-      scale:1,
-      duration:3
-  })
+import React from 'react';
 
-    // Cleanup on unmount
-    return () => {
-      scroll.destroy();
-    };
-  }, []);
+function App() {
+
+  
   return (
-  <div id='main'>
+  <>
   <NavBar/>
   <Hero/>
   <About/>
   <MyWork/>
   <Contact/>
   <Footer/>
- </div>
+  </>
   )
 }
 
