cd3895469e0e17c2107b82ad95f4e27be1f6e8fb
Syed Farman Ali <88277123+FARMAN9@users.noreply.github.com>
2025-02-07 22:11:56 +0530

    NEED FIX MOBILE UI

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Footer.css";
 import ali from "../../assets/ali.png";
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 function Footer() {
   return (
@@ -9,37 +9,38 @@ function Footer() {
       <div className="footer-top">
         <div className="footer-top-left">
           <div className="logo">
-          <AnchorLink className='anchor-link' offset={50} href='#home'>
-            <h1>FARMAN</h1>
-            <img src={ali} alt="" />
+            <AnchorLink className="anchor-link" offset={50} href="#home">
+              <h1>FARMAN</h1>
+              <img src={ali} alt="" />
             </AnchorLink>
           </div>
-       
         </div>
         <p>
-        I wanted to take a moment to thank you for visiting my portfolio. Your time and interest in my work are truly appreciated.
-        If you have any feedback, questions, or opportunities you'd like to discuss, please don't hesitate to reach out. I'm always eager to connect and explore new possibilities.
-          </p>
-        <div className="footer-top-right">
-         
-        </div>
-       
-        </div>
-        <hr />
+          I wanted to take a moment to thank you for visiting my portfolio. Your
+          time and interest in my work are truly appreciated. If you have any
+          feedback, questions, or opportunities you'd like to discuss, please
+          don't hesitate to reach out. I'm always eager to connect and explore
+          new possibilities.
+        </p>
+        <div className="footer-top-right"></div>
+      </div>
+      <hr className="footer-hr" />
 
-        <div className="footer-bottom">
-          <p className="footer-bottm-left">
+      <div className="footer-bottom">
+        <p className="footer-bottm-left">
+          {" "}
+          © 2024 Syed Farman Ali. All rights reserved.{" "}
+        </p>
+
+        <div className="footer-bottom-right">
+          <p>Term of Services</p>
+          <p>Privacy Policy</p>
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
             {" "}
-            © 2024 Syed Farman Ali. All rights reserved.{" "}
-          </p>
-
-          <div className="footer-bottom-right">
-            <p>Term of Services</p>
-            <p>Privacy Policy</p>
-            <AnchorLink className='anchor-link' offset={50} href='#contact'> <p>Connect with me</p></AnchorLink>
-          </div>
+            <p>Connect with me</p>
+          </AnchorLink>
         </div>
-     
+      </div>
     </div>
   );
 }
--- src/Components/Navbar/NavBar.jsx
+++ src/Components/Navbar/NavBar.jsx
@@ -1,46 +1,75 @@
-import React, { useRef, useState,useEffect } from 'react';
-import './Navbar.css';
-import ali from '../../assets/ali.png'
-import backgound from '../../assets/nav_underline.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
-import menu_open from '../../assets/menu_open.svg'
-import menu_close from '../../assets/menu_close.svg'
-
-
+import React, { useRef, useState, useEffect } from "react";
+import "./Navbar.css";
+import ali from "../../assets/ali.png";
+import backgound from "../../assets/nav_underline.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import menu_open from "../../assets/menu_open.svg";
+import menu_close from "../../assets/menu_close.svg";
 
 function NavBar() {
- 
-
-
-  const [menu,setMenu]=useState('home')
-  const menuRef =useRef();
-  const openMenu=()=>{
-    menuRef.current.style.right='0'
-  }
-  const closeMenu=()=>{
-    menuRef.current.style.right='-350px'
-  }
+  const [menu, setMenu] = useState("home");
+  const menuRef = useRef();
+  const openMenu = () => {
+    menuRef.current.style.right = "0";
+  };
+  const closeMenu = () => {
+    menuRef.current.style.right = "-350px";
+  };
   return (
-    <nav className='navbar'>
-      <div className='logo'>
-      <AnchorLink className='anchor-link' href='#home'>
-      <h1>FARMAN</h1>
-      <img src={ali} alt="" />
-      </AnchorLink>
+    <nav className="navbar">
+      <div className="logo">
+        <AnchorLink className="anchor-link" href="#home">
+          <h1>FARMAN</h1>
+          <img src={ali} alt="" />
+        </AnchorLink>
       </div>
-      <img src={menu_open}  onClick={openMenu} alt="" className='nav-mob-open' />
-           
-            <ul ref={menuRef} className='nav-menu'>
-            <img src={menu_close} alt="" className='nav-mob-close' onClick={closeMenu}/>
-             <li> <AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu('home')}>Home</p> </AnchorLink>{menu ==='home' ? <img src ={backgound} alt=''/>:<></> }</li>
-             <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu('about')}>About Me</p></AnchorLink>{menu ==='about' ? <img src ={backgound} alt=''/>:<></> }</li>
-             <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={()=>setMenu('work')}>Portfolio</p></AnchorLink>{menu ==='work' ? <img src ={backgound} alt=''/>:<></> }</li>
-             <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu('contact')}>Contact</p></AnchorLink>{menu ==='contact' ? <img src ={backgound} alt=''/>:<></> }</li>
-            </ul>
-            <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-       
+      <img
+        src={menu_open}
+        onClick={openMenu}
+        alt=""
+        className="nav-mob-open d-md-none"
+      />
+
+      <ul ref={menuRef} className="nav-menu d-none d-md-flex">
+        <img
+          src={menu_close}
+          alt=""
+          className="nav-mob-close d-md-none"
+          onClick={closeMenu}
+        />
+        <li>
+          {" "}
+          <AnchorLink className="anchor-link" href="#home">
+            <p onClick={() => setMenu("home")}>Home</p>{" "}
+          </AnchorLink>
+          {menu === "home" ? <img src={backgound} alt="" /> : <></>}
+        </li>
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#about">
+            <p onClick={() => setMenu("about")}>About Me</p>
+          </AnchorLink>
+          {menu === "about" ? <img src={backgound} alt="" /> : <></>}
+        </li>
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#work">
+            <p onClick={() => setMenu("work")}>Portfolio</p>
+          </AnchorLink>
+          {menu === "work" ? <img src={backgound} alt="" /> : <></>}
+        </li>
+        <li>
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            <p onClick={() => setMenu("contact")}>Contact</p>
+          </AnchorLink>
+          {menu === "contact" ? <img src={backgound} alt="" /> : <></>}
+        </li>
+      </ul>
+      <div className="nav-connect">
+        <AnchorLink className="anchor-link" offset={50} href="#contact">
+          Connect With Me
+        </AnchorLink>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
